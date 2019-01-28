e0891cf696d79452ffb188d2559734c229262c44
Raphael Peters <rappet@rappet.de>
2019-01-28 20:15:59 +0100

    Simple hello world server.

---
+++ src/errors.rs
@@ -0,0 +1,9 @@
+error_chain! {
+    foreign_links {
+        AddrParse(std::net::AddrParseError);
+    }
+
+    errors {
+        
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+#[macro_use]
+extern crate error_chain;
+
+extern crate hyper;
+
+use hyper::{Body, Response, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::Future;
+use hyper::header;
+
+mod errors;
+use errors::*;
+
+fn main() {
+    env_logger::init();
+
+    // Error handling code.
+    if let Err(ref e) = run() {
+        use std::io::Write;
+
+        error!("{}", e);
+
+        for e in e.iter().skip(1) {
+            error!("caused by: {}", e);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            error!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let addr = "[::]:8080".parse()?;
+
+    let make_service = || {
+        service_fn_ok(|req| {
+            info!("Request from {:?} for {}", req.headers()[header::HOST], req.uri());
+            Response::new(Body::from("Hello, World!"))
+        })
+    };
+
+    let server = Server::bind(&addr)
+        .serve(make_service)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
+
+    Ok(())
+}
